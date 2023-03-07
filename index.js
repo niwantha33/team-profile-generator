d9f9e744fb07da9771f8c8395bf8430fcda51943
niwantha33 <niwantha33@gmail.com>
2023-03-07 22:27:43 +0000

    added prompt.

---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+// user modules
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+
+// outputs 
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
