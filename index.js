af24c2d1239d5ad87acacbc61bc6b481ab948b9c
niwantha33 <niwantha33@gmail.com>
2023-03-07 23:31:50 +0000

    added prompt.

--- index.js
+++ index.js
@@ -18,3 +18,52 @@ const render = require("./src/page-template.js");
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the team manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team manager's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the team manager's email?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the team manager's office number?",
+    },
+
+//   .then((data) => {
+//     const manager = new Manager(
+//       data.name,
+//       data.id,
+//       data.email,
+//       data.officeNumber
+//     );  
+    
+    
+//   });
+
+    {
+      type: "confirm",
+      name: "again",
+      message: "Do you want to add another employee?",
+    },
+  ])
+  .then((again) =>{
+
+  if (again) {
+    console.log(again);
+  } else {
+    // Generate HTML here
+    console.log("Team profile generated!");
+  }
+})
