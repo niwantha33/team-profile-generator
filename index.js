fe0b1fd9990ec5776c4e8e17bbfd2cd153a84c89
niwantha33 <niwantha33@gmail.com>
2023-03-13 12:09:18 +0000

    completed test.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const Intern = require("./lib/Intern");
 
 // outputs 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
+
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
